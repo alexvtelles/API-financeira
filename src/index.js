44f2bc41cad2a96691faca60527ad8084479370e
Alex Telles <outdsystem@gmail.com>
2022-10-21 09:55:25 -0300

    feat: routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * CPF -> String
+ * name -> string
+ * id -> uuid
+ * statement -> []
+ */
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+
+  const customersAlreadyExist = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customersAlreadyExist) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.get("/statement", (req, res) => {
+  const { cpf } = req.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return res.status(404).json({ error: "Customer not found!" });
+  }
+
+  return res.json(customer.statement);
+});
 
 app.listen(3333);
